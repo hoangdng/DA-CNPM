848d1cc39620c639f9d261eaee19850fd72678fd
Hoàng Trần <nguyenhoang7693@gmail.com>
2020-05-30 00:06:52 +0700

    Edit Home and AboutUs page

--- Startup.cs
+++ Startup.cs
@@ -76,7 +76,7 @@ namespace PetWeb
             });
 
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
