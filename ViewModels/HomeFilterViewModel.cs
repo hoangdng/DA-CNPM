8d0fca34036ee3dbe7061d6b582b3ae8a76d9a1f
Hoàng Trần <nguyenhoang7693@gmail.com>
2020-06-03 09:59:51 +0700

    Add filter by PostedDate, Animal, Category

---
+++ ViewModels/HomeFilterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PetWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetWeb.ViewModels
+{
+    public class HomeFilterViewModel
+    {
+        public List<Post> Posts { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
